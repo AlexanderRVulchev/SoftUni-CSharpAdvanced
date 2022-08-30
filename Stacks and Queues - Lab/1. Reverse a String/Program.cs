648ba52703a7ceacab627167cc0e6ca911db1456
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-30 17:46:58 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/1. Reverse a String/Program.cs
@@ -0,0 +1,29 @@
+﻿//Create a program that:
+//•	Reads an input string
+//•	Reverses it using a Stack<T>
+//•	Prints the result back at the terminal
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        Stack<char> lettersStack = new Stack<char>();
+        for (int i = 0; i < input.Length; i++)
+        {
+            lettersStack.Push(input[i]);
+        }
+
+        StringBuilder result = new StringBuilder();
+        while(lettersStack.Count != 0)
+        {
+            result.Append(lettersStack.Pop());
+        }
+        Console.WriteLine(result);        
+    }
+}
