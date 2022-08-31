ef54aaa9f631893f51bf2e14637bdca37a704edb
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-31 07:36:26 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿//Create a program that:
+//•	Reads an array of integers and adds them to a queue
+//•	Prints the even numbers separated by ", "
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
+{
+    static void Main()
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Queue<int> nums = new Queue<int>(input);
+        List<int> evenNumbers = new List<int>();
+        
+        while (nums.Count != 0)
+        {
+            int currentNumber = nums.Dequeue();
+            if (currentNumber % 2 == 0)
+            {
+                evenNumbers.Add(currentNumber);
+            }
+        }
+        Console.WriteLine(String.Join(", ", evenNumbers));
+    }
+}
