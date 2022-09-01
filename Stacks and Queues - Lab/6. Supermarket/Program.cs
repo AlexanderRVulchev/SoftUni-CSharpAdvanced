e29010f205d2fe297bf1862204f94415415e808f
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-01 07:40:51 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,30 @@
+﻿//Reads an input consisting of a name and adds it to a queue until "End" is received.
+//If you receive "Paid", print every customer name and empty the queue, otherwise,
+//we receive a client and we have to add him to the queue.
+//When we receive "End" we have to print the count of the remaining people in the queue in the format:
+//"{count} people remaining.".
+
+using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main()
+    {
+        string input;
+        var names = new Queue<string>();
+        while ((input = Console.ReadLine()) != "End")
+        {
+            if (input == "Paid")
+            {
+                while (names.Count != 0)
+                {
+                    Console.WriteLine(names.Dequeue());
+                }
+            }
+            else
+                names.Enqueue(input);
+        }
+        Console.WriteLine($"{names.Count} people remaining.");
+    }
+}
