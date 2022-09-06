ca2516f472c01e217bac4e2369ff5aea03c5651c
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-06 16:21:15 +0300

    Add files via upload

---
+++ Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,27 @@
+﻿//Create a program that finds the sum of matrix primary diagonal.
+
+using System;
+using System.Linq;
+
+internal class Program
+{
+    static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[size, size];
+
+        for (int row = 0; row < size; row++)
+        {
+            int[] userLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int col = 0; col < userLine.Length; col++)
+            {
+                matrix[row, col] = userLine[col];
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < size; i++)
+            sum += matrix[i, i];
+        Console.WriteLine(sum);
+    }
+}
