beffa1f60e8cf19ac70f8ffc630b9405b8d21e38
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-06 16:19:17 +0300

    Add files via upload

---
+++ Multidimensional Arrays - Lab/2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,35 @@
+﻿//Create a program that reads a matrix from the console and prints the sum for each column.
+//On the first line, you will get matrix rows.
+//On the next rows lines, you will get elements for each column separated with a space. 
+
+using System;
+using System.Linq;
+
+internal class Program
+{
+    static void Main()
+    {
+        string rowColInfo = Console.ReadLine();
+        int rows = rowColInfo.Split(", ").Select(int.Parse).First();
+        int columns = rowColInfo.Split(", ").Select(int.Parse).Last();
+        int[,] matrix = new int[rows, columns];
+        for (int row = 0; row < rows; row++)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int num = 0; num < input.Length; num++)
+            {
+                matrix[row, num] = input[num];
+            }
+        }
+
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            int columnSum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                columnSum += matrix[row, col];
+            }
+            Console.WriteLine(columnSum);
+        }
+    }
+}
