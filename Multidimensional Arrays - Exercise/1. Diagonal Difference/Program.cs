bfeccdd3c90f651b78c0206ee9999337ab096ec5
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-08 07:58:39 +0300

    Add files via upload

---
+++ Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,36 @@
+﻿//Create a program that finds the difference between the sums of the square matrix diagonals (absolute value).
+
+using System;
+using System.Linq;
+
+internal class Program
+{
+    static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+        int[,] matrix = GetMatrixData(size);
+        int primarySum = 0;
+        int secondarySum = 0;
+        
+        for (int i = 0; i < size; i++)
+        {
+            primarySum += matrix[i, i];
+            secondarySum += matrix[i, size - i - 1];
+        }
+        Console.WriteLine(Math.Abs(primarySum - secondarySum));
+    }
+
+    private static int[,] GetMatrixData(int size)
+    {
+        int[,] matrix = new int[size, size];
+        for (int row = 0; row < size; row++)
+        {
+            int[] userLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int col = 0; col < size; col++)
+            {
+                matrix[row, col] = userLine[col];
+            }
+        }
+        return matrix;
+    }
+}
