922d26f9d8847bd97086f1815e3684094ab9c0c3
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-12 08:37:52 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,32 @@
+﻿//Create a program, which reads a name of a student and his/her grades
+//and adds them to the student record,
+//then prints the students' names with their grades and their average grade.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int numberOfEntries = int.Parse(Console.ReadLine());
+        var gradesByName = new Dictionary<string, List<decimal>>();
+        for (int i = 0; i < numberOfEntries; i++)
+        {
+            string entry = Console.ReadLine();
+            string name = entry.Split().First();
+            decimal grade = decimal.Parse(entry.Split().Last());
+
+            if (!gradesByName.ContainsKey(name))
+                gradesByName.Add(name, new List<decimal>());
+            gradesByName[name].Add(grade);
+        }
+        foreach (var name in gradesByName)
+        {
+            Console.WriteLine($"{name.Key:f2} -> "
+                + String.Join(" ", name.Value.Select(x => $"{x:f2}"))
+                + $" (avg: {name.Value.Average():f2})");
+        }
+    }
+}
