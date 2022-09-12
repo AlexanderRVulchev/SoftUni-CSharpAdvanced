cb8fb434cf50034f02e463b1663704c879d414e5
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-12 08:37:20 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,24 @@
+﻿//Create a program that counts in a given array of double values the number of occurrences of each value. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        double[] sequence = Console.ReadLine().Split().Select(double.Parse).ToArray();
+        var occurences = new Dictionary<double, int>();
+        foreach (var num in sequence)
+        {
+            if (!occurences.ContainsKey(num))
+                occurences.Add(num, 0);
+            occurences[num]++;
+        }
+        foreach (var occurence in occurences)
+        {
+            Console.WriteLine($"{occurence.Key} - {occurence.Value} times");
+        }
+    }
+}
