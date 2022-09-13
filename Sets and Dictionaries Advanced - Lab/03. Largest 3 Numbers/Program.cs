b7097822526408b2fb3f5b6bd0c2c23bc8dc9ea4
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-13 20:19:16 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿//Read a list of integers and print the largest 3 of them.
+//If there are less than 3, print all of them.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] result = nums.OrderByDescending(x => x).Take(3).ToArray();
+        Console.WriteLine(String.Join(" ", result));
+    }
+}
