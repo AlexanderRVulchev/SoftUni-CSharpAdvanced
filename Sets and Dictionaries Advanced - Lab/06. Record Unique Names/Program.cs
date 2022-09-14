c3853008fc34d99ec9df8c287051999477dfbfbe
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-14 07:16:52 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a program, which will take a list of names and print only the unique names in the list.
+
+using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main()
+    {
+        HashSet<string> names = new HashSet<string>();
+        int numberOfNames = int.Parse(Console.ReadLine());
+        for (int i = 0; i < numberOfNames; i++)
+        {
+            names.Add(Console.ReadLine());
+        }
+        Console.WriteLine(string.Join("\n", names));
+    }
+}
