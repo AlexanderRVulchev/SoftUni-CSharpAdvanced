165d983ac632e39b1eef579012f26ea9081ebad8
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-15 18:29:48 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,26 @@
+﻿//Create a program that keeps all the unique chemical elements.
+//On the first line, you will be given a number n -
+//the count of input lines that you are going to receive.
+//On the next n lines, you will be receiving chemical compounds, separated by a single space.
+//Your task is to print all the unique ones in ascending order:
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        SortedSet<string> elements = new SortedSet<string>();
+        int numberOfLines = int.Parse(Console.ReadLine());
+        for (int line = 0; line < numberOfLines; line++)
+        {
+            string[] input = Console.ReadLine().Split();
+            for (int element = 0; element < input.Length; element++)
+            {
+                elements.Add(input[element]);
+            }
+        }
+        Console.WriteLine(string.Join(" ", elements));
+    }
+}
