46b4c45ccffc1c878ec5ebbf2c0da4f3328e9610
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-16 21:13:35 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,26 @@
+﻿//Create a program that reads some text from the console
+//and counts the occurrences of each character in it.
+//Print the results in alphabetical (lexicographical) order.
+
+using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main()
+    {
+        var dictSymbols = new SortedDictionary<char, int>();
+        string input = Console.ReadLine();
+        for (int i = 0; i < input.Length; i++)
+        {
+            char symbol = input[i];
+            if (!dictSymbols.ContainsKey(symbol))
+                dictSymbols.Add(symbol, 0);
+            dictSymbols[symbol]++;
+        }
+        foreach (var kvp in dictSymbols)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+        }
+    }
+}
