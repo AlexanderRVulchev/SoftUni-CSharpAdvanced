b955a9b7f1bf82e2fb48d9f44ce64025e2a9ff06
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-18 07:57:14 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Lab/01. Odd Lines/OddLines.cs
@@ -0,0 +1,38 @@
+﻿//Write a program that reads a text file (e. g. input.txt) and writes every odd line in another file.
+//Line numbers start from 0. 
+
+namespace OddLines
+{
+    using System.IO;
+
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                int counter = 0;
+                StreamWriter writer = new StreamWriter(outputFilePath);
+                string input;
+                using (writer)
+                {
+                    while ((input = reader.ReadLine()) != null)
+                    {
+                        if (counter % 2 == 1)
+                            writer.WriteLine(input);
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
