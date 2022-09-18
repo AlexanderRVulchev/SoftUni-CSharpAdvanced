b110ceabfcdb17a40c3e9800c12b3ef7a448b3bd
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-18 07:58:16 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Lab/02. LineNumbers/LineNumbers.cs
@@ -0,0 +1,36 @@
+﻿//Write a program that reads a text file (e. g. input.txt)
+//and inserts line numbers in front of each of its lines.
+//The result should be written to another text file (e. g. output.txt).
+//Use StreamReader and StreamWriter.
+
+namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using (var reader = new StreamReader(inputFilePath))
+            {                
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+                    int counter = 1;
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
