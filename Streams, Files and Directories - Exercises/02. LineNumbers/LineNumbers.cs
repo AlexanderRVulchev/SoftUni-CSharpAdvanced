cde87ab3793a40afbcae2037ef8388f55e4688d8
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-20 18:35:31 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Exercises/02. LineNumbers/LineNumbers.cs
@@ -0,0 +1,42 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Text.RegularExpressions;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string output = String.Empty;
+            int lineCounter = 1;
+            using (var reader = new StreamReader(inputFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    MatchCollection letterMatches = Regex.Matches(line, @"[A-Za-z]");
+                    MatchCollection punctuationMatches = Regex.Matches(line, @"[-.!,?']");
+                    output = output 
+                        + $"Line {lineCounter}: " 
+                        + line 
+                        + $" ({letterMatches.Count})({punctuationMatches.Count})" 
+                        + "\n";
+                    lineCounter++;
+                }
+            }
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                writer.Write(output);
+            }
+        }
+    }
+}
