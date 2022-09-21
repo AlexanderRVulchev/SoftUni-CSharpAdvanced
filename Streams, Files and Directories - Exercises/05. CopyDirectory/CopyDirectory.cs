5d92b6b56a6e14fdcb5e7123aa2df4bf4fc2e972
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-21 07:37:01 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Exercises/05. CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,23 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+    
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            string[] files = Directory.GetFiles(inputPath);
+            Directory.Move(inputPath, outputPath);
+        }
+    }
+}
