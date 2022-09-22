8edb435916632b7a695aa6b49b6284c2253f93b3
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-22 16:28:23 +0300

    Add files via upload

---
+++ Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿//Create a program that reads one line of integers separated by ", ".
+//Then prints the even numbers of that sequence sorted in increasing order.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(
+            String.Join(", ",
+            Console.ReadLine()
+            .Split(", ")
+            .Select(int.Parse)
+            .Where(x => x % 2 == 0)
+            .OrderBy(x => x)
+            ));
+    }
+}
