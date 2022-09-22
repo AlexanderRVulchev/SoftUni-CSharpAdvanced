12a4e546c9326363de69709926b85102294afaf2
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-22 16:28:49 +0300

    Add files via upload

---
+++ Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a program that reads a line of integers separated by ", ".
+//Print on two lines the count of numbers and their sum.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static Action<int[]> output = x => Console.WriteLine($"{x.Count()}\n{x.Sum()}");
+
+    static void Main()
+    {
+        output(Console.ReadLine()
+              .Split(", ")
+              .Select(int.Parse)
+              .ToArray());
+    }
+}
