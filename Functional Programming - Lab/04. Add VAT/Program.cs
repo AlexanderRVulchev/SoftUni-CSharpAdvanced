deb4407b01cae957d52d07cedf8b906d0c4f31e6
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-22 16:29:36 +0300

    Add files via upload

---
+++ Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,25 @@
+﻿//Create a program that reads one line of double prices separated by ", ".
+//Print the prices with added VAT for all of them.
+//Format them to 2 signs after the decimal point.
+//The order of the prices must be the same.
+//VAT is equal to 20% of the price.
+
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static Func<string, string> addVat = x 
+        => $"{double.Parse(x) * 1.2:f2}";
+
+    static void Main()
+    {
+        Console.WriteLine(
+            string.Join("\n",
+            Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(addVat)
+            ));
+    }
+}
