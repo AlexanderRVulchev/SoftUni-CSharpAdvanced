66f8b6eee372d7abfc08529ea8034d1360f5a770
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-22 16:29:15 +0300

    Add files via upload

---
+++ Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,21 @@
+﻿//Create a program that reads a line of text from the console.
+//Print all the words that start with an uppercase letter in the same order you've received them in the text.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static Func<string, bool> isUppercase = x
+        => x[0] == x.ToUpper()[0];
+
+    static void Main()
+    {                
+            Console.WriteLine(                
+                String.Join("\n",
+                Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(isUppercase)
+                ));
+    }
+}
