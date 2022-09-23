12fca57495aecf3e6ed417251ef13ada04aeb367
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-23 21:47:18 +0300

    Add files via upload

---
+++ Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,22 @@
+﻿//Create a simple program that reads from the console a set of integers
+//and prints back on the console the smallest number from the collection.
+//Use Func<T, T>.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Func<int[], int> calcMin =
+            x => x.Min();
+
+        int[] nums = Console.ReadLine()
+            .Split()
+            .Select(x => int.Parse(x))
+            .ToArray();
+
+        Console.WriteLine(calcMin(nums));
+    }
+}
