3cb226ae641eb5094918a03f62930515007d12eb
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-23 21:45:57 +0300

    Add files via upload

---
+++ Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a program that reads a collection of strings from the console
+//and then prints them onto the console.
+//Each name should be printed on a new line.Use Action<T>.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Action<string[]> print =
+            x => Console.WriteLine(string.Join("\n", x));
+
+        string[] names = Console.ReadLine().Split();
+        print(names);
+    }
+}
