396f653d53045bf5f53aaa0b19b4900b7b88419d
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-23 21:46:25 +0300

    Add files via upload

---
+++ Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a program that reads a collection of names as strings from the console,
+//appends "Sir" in front of every name,
+//and prints it back on the console. Use Action<T>.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Action<string> print =
+            x => Console.WriteLine("Sir " + x);
+
+        string[] names = Console.ReadLine().Split();
+        foreach (string name in names)
+            print(name);
+    }
+}
