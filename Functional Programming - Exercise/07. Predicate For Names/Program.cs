feaace35e376c0c6aea7f239446a826fd93b767a
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-24 07:19:30 +0300

    Add files via upload

---
+++ Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,20 @@
+﻿//Write a program that filters a list of names according to their length.
+//On the first line, you will be given an integer n, representing a name's length.
+//On the second line, you will be given some names as strings separated by space.
+//Write a function that prints only the names whose length is less than or equal to n.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int maxLength = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split();
+        Predicate<string> isValidLength = x
+            => x.Length <= maxLength;
+        Console.WriteLine(string.Join("\n", names
+            .Where(x => isValidLength(x))));
+    }
+}
