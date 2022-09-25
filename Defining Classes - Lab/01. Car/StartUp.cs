87fc2f580b79b0f741ab133e92f6e882920ed675
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-25 17:57:34 +0300

    Add files via upload

---
+++ Defining Classes - Lab/01. Car/StartUp.cs
@@ -0,0 +1,34 @@
+﻿//NOTE: You need a StartUp class with the namespace CarManufacturer.
+//Create a class named Car.The class should have private fields for:
+//•	make: string
+//•	model: string
+//•	year: int
+//The class should also have public properties for:
+//•	Make: string
+//•	Model: string
+//•	Year: int
+
+
+using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        class Car
+        {
+            private string make;
+            private string model;
+            private int year;
+
+            public string Make { get { return make; } set { make = value; } }
+            public string Model { get { return model; } set { model = value; } }
+            public int Year { get { return year; } set { year = value; } }
+        }
+
+        static void Main()
+        {
+            
+        }
+    }
+}
