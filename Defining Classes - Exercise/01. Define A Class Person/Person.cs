f4e6f5037389b9cffae18ca20c58f2903e2cc2c3
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-26 18:16:16 +0300

    Add files via upload

---
+++ Defining Classes - Exercise/01. Define A Class Person/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+    }
+}
---
+++ Defining Classes - Exercise/01. Define A Class Person/StartUp.cs
@@ -0,0 +1,18 @@
+﻿//Define a class Person with private fields for name and age and public properties Name and Age.
+
+using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person peter = new Person();
+            peter.Name = "Peter";
+            peter.Age = 20;
+            Console.WriteLine(peter.Name);
+            Console.WriteLine(peter.Age);
+        }
+    }
+}
