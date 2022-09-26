f59dc931e0b204eb41570a5d05ca704ccb2af257
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-26 18:16:59 +0300

    Add files via upload

---
+++ Defining Classes - Exercise/02. Creating Constructors/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age) 
+            : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+            : this(age)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,24 @@
+﻿//Add 3 constructors to the Person class from the last task.
+//Use constructor chaining to reuse code:
+//•	The first should take no arguments and produce a person
+//with the name "No name" and age = 1. 
+//•	The second should accept only an integer number for the age
+//and produce a person with the name "No name" and age equal to the passed parameter.
+//•	The third one should accept a string for the name and an integer for the age
+//and should produce a person with the given name and age.
+
+
+using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person john = new Person("John", 22);
+            Console.WriteLine(john.Name);
+            Console.WriteLine(john.Age);
+        }
+    }
+}
