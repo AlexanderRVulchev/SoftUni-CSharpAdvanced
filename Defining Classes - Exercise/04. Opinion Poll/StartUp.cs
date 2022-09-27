01ec40f8aa31d6b13f85f54e022f84005f15695f
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-27 20:24:25 +0300

    Add files via upload

---
+++ Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,34 @@
+﻿//Using the Person class, write a program that reads from the console
+//N lines of personal information and then prints all people,
+//whose age is more than 30 years, sorted in alphabetical order.
+
+using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Family family = new Family();
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                string[] nameAndAge = Console.ReadLine().Split();
+                string name = nameAndAge[0];
+                int age = int.Parse(nameAndAge[1]);
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+            
+            Person[] filteredPeople = family.People
+                .FindAll(p => p.Age > 30)
+                .OrderBy(p => p.Name)
+                .ToArray();
+
+            foreach (Person person in filteredPeople)
+                Console.WriteLine(person);
+        }
+    }
+}
