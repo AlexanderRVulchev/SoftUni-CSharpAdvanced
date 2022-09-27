488125111258c94d6ada2aa937df2c59c657c8ac
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-27 20:25:00 +0300

    Add files via upload

---
+++ Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public static int GetDateDifference(string first, string second)
+        {
+            DateTime firstDate = DateTime.ParseExact(first, "yyyy MM dd", CultureInfo.InvariantCulture);
+            DateTime secondDate = DateTime.ParseExact(second, "yyyy MM dd", CultureInfo.InvariantCulture);
+            TimeSpan dateDifference = firstDate - secondDate;
+            return Math.Abs(dateDifference.Days);
+        }
+    }
+}
---
+++ Defining Classes - Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+            int differenceInDays = DateModifier.GetDateDifference(firstDate, secondDate);
+            Console.WriteLine(differenceInDays);
+        }
+    }
+}
