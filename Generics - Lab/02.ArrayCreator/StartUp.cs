fc63f2743b1b9f2fae27d1b3b73491e2dc6efaf8
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-29 07:44:42 +0300

    Add files via upload

---
+++ Generics - Lab/02.ArrayCreator/StartUp.cs
@@ -0,0 +1,19 @@
+﻿//Create a class ArrayCreator with a method and a single overload to it:
+//•	static T[] Create(int length, T item)
+//The method should return an array with the given length
+//and every element should be set to the given default item.
+
+
+using System;
+
+namespace GenericArrayCreator
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+        }
+    }
+}
