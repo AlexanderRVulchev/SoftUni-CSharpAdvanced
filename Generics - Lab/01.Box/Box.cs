5f97ab842aa157f7bd4762f88a1b545b7de60f56
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-29 07:44:15 +0300

    Add files via upload

---
+++ Generics - Lab/01.Box/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> elements = new Stack<T>();
+
+        public int Count { get { return elements.Count; } }
+
+        public void Add(T element)
+        {
+            this.elements.Push(element);
+        }
+
+        public T Remove()
+        {
+            return this.elements.Pop();
+        }
+    }
+}
---
+++ Generics - Lab/01.Box/StartUp.cs
@@ -0,0 +1,25 @@
+﻿//Create a class Box<> that can store anything. It should have two public methods:
+//•	void Add(element) – adds an element on the top of the list.
+//•	element Remove() – removes the topmost element.
+//•	int Count { get; }
+
+
+using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+            Console.WriteLine(box.Remove());
+            box.Add(4);
+            box.Add(5);
+            Console.WriteLine(box.Remove());
+        }
+    }
+}
