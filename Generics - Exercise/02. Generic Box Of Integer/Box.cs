d5387d632a8158a7cb1d52a09be28f525145912a
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-01 11:38:51 +0300

    Add files via upload

---
+++ Generics - Exercise/02. Generic Box Of Integer/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        private T boxValue;
+
+        public T BoxValue { get { return boxValue; } set { boxValue = value; } }
+
+        public Box(T entry)
+        {
+            boxValue = entry;
+        }
+
+        public override string ToString() =>        
+        $"{this.BoxValue.GetType()}: {this.BoxValue}";        
+    }
+}
---
+++ Generics - Exercise/02. Generic Box Of Integer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int numberOfBoxes = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfBoxes; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(input);
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
