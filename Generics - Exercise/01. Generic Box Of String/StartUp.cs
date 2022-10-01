d0c0e57855c8842b86632da4f44a8f0adf945952
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-01 11:38:26 +0300

    Add files via upload

---
+++ Generics - Exercise/01. Generic Box Of String/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int numberOfBoxes = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfBoxes; i++)
+            {
+                var input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
