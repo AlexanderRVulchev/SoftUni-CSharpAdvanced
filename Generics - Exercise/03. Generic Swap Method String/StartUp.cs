385a5033737d0515062b7aca1c2eb6971b804771
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-01 11:39:14 +0300

    Add files via upload

---
+++ Generics - Exercise/03. Generic Swap Method String/StartUp.cs
@@ -0,0 +1,45 @@
+﻿//Create a generic method that receives a list,
+//containing any type of data and swaps the elements at two given indexes.
+//Input
+//•	On the first line, you will read n number of boxes of type string and add them to the list.
+//•	On the next line, however, you will receive a swap command consisting of two indexes.
+//Output
+//•	Use the method you've created to swap the elements
+//that correspond to the given indexes and print each element in the list.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            List<string> items = new List<string>();
+            int numberOfItems = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                items.Add(Console.ReadLine());
+            }
+            string input = Console.ReadLine();
+            int firstIndex = input.Split().Select(x => int.Parse(x)).First();
+            int secondIndex = input.Split().Select(x => int.Parse(x)).Last();
+            List<string> swappedList = Swap(items, firstIndex, secondIndex);
+            foreach (var item in swappedList)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+
+        public static List<T> Swap<T>(List<T> items, int firstIndex, int secondIndex)
+        {
+            T temp = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = temp;
+            return items;
+        }
+    }
+}
