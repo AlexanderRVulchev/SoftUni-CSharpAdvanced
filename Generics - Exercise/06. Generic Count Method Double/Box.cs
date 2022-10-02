132884c16ef25ed4e77754056523d0c4fc95f3b0
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-02 09:24:42 +0300

    Add files via upload

---
+++ Generics - Exercise/06. Generic Count Method Double/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericCountMethodDouble
+{
+    public class Box<T>
+    {
+        private T boxValue;
+
+        public T BoxValue { get { return boxValue; } set { boxValue = value; } }
+
+        public Box(T entry)
+        {
+            this.boxValue = entry;
+        }
+
+        public static int GetCountOfLargerElements(List<Box<T>> list, T item)
+        {            
+            Comparer<T> comparer = Comparer<T>.Default;            
+            int result = list.Where(x => comparer.Compare(x.BoxValue, item) > 0).ToList().Count;
+            return result;
+        }
+    }
+}
---
+++ Generics - Exercise/06. Generic Count Method Double/StartUp.cs
@@ -0,0 +1,24 @@
+﻿//Use the description of the previous problem, but now, test your list of generic boxes with doubles.
+
+
+using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodDouble
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            List<Box<double>> boxes = new List<Box<double>>();
+            int numberOfEntries = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                double entry = double.Parse(Console.ReadLine());
+                boxes.Add(new Box<double>(entry));
+            }
+            double item = double.Parse(Console.ReadLine());
+            Console.WriteLine(Box<double>.GetCountOfLargerElements(boxes, item));
+        }
+    }
+}
