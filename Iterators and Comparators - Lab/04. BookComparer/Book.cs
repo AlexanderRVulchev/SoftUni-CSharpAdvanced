b15d1a53c45266207d7eab54a3d41d427f017f27
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-03 21:00:05 +0300

    Add files via upload

---
+++ Iterators and Comparators - Lab/04. BookComparer/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = new List<string>(authors);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+
+
+        public int CompareTo([AllowNull] Book other)
+        {
+            BookComparator bc = new BookComparator();
+            return bc.Compare(this, other);
+        }
+
+
+    }
+}
