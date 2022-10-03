25d81b58241fd08821fcbdc55359bdf303fafb46
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-03 21:00:20 +0300

    Add files via upload

---
+++ Iterators and Comparators - Lab/04. BookComparer/BookComparator.cs
@@ -0,0 +1,19 @@
+﻿using IteratorsAndComparators;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare([AllowNull] Book x, [AllowNull] Book y)
+        {
+            int result = x.Title.CompareTo(y.Title);
+            if (result == 0)
+                result = y.Year.CompareTo(x.Year);
+            return result;
+        }
+    }
+}
