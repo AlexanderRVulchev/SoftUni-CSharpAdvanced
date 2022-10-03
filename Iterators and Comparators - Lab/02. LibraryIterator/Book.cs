404e5229923a1421ba0ca6bd74014e5560ed0b5e
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-03 20:58:47 +0300

    Add files via upload

---
+++ Iterators and Comparators - Lab/02. LibraryIterator/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = new List<string>(authors);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year} - {string.Join(", ", this.Authors)}";
+        }
+    }
+}
