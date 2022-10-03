c5c71bf3f2e3b2e91cbed316ced3a13a4ff67788
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-03 20:57:46 +0300

    Add files via upload

---
+++ Iterators and Comparators - Lab/01. Library/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    class Library
+    {
+        public List<Book> Books { get; set; }
+
+        public Library(params Book[] books)
+        {
+            this.Books = new List<Book>(books);
+        }
+    }
+}
