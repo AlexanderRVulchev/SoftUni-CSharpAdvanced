f42148bc6098e01fe4d5f9f7ed3a1cf2045d87cb
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-04 19:10:11 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private int currentIndex;
+
+        public List<T> Elements;
+        
+        public ListyIterator(params T[] entries)
+        {
+            this.Elements = new List<T>(entries);
+            this.currentIndex = 0;
+        }
+
+        public bool Move()
+        {
+            if (this.currentIndex < this.Elements.Count - 1)
+            {
+                this.currentIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return this.currentIndex < this.Elements.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (this.Elements.Count != 0)
+                Console.WriteLine(this.Elements[currentIndex]);
+            else
+                Console.WriteLine("Invalid Operation!");
+        }
+    }
+}
