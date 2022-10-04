e8ea5370fbf7d4b3e4755d4d620f91583d10305f
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-04 19:11:43 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/02. Collection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] entries = Console.ReadLine().Split().Skip(1).ToArray();
+            ListyIterator<string> listy = new ListyIterator<string>(entries);
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                switch(input)
+                {
+                    case "Move": Console.WriteLine(listy.Move()); break;
+                    case "HasNext": Console.WriteLine(listy.HasNext()); break;
+                    case "Print": listy.Print(); break;
+                    case "PrintAll": listy.PrintAll(); break;
+                }
+            }
+
+        }
+    }
+}
