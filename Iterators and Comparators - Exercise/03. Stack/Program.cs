1c48da0f77b5ae01733b635d5a054a8a87f52919
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-04 19:12:59 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/03. Stack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Stack
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stack<string> stack = new Stack<string>();
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] command = input
+                    .Split(new string[] {" ", ", "}, StringSplitOptions.RemoveEmptyEntries);
+                if (command[0] == "Push")
+                {
+                    string[] elements = command.Skip(1).ToArray();
+                    stack.Push(elements);
+                }
+                else if (command[0] == "Pop")
+                    stack.Pop();                    
+            }
+            foreach (var item in stack)
+                Console.WriteLine(item);
+            foreach (var item in stack)
+                Console.WriteLine(item);
+        }
+    }
+}
