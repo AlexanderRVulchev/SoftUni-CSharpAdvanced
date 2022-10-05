495b6bea1d88d5392d366dd230d740152b5d19df
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-05 19:58:27 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/05. Comparing Objects/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set;}
+
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public int CompareTo([AllowNull] Person other)
+        {
+            if (this.Name == other.Name && this.Age == other.Age && this.Town == other.Town)
+                return 0;
+            else
+                return 1;
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/05. Comparing Objects/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComparingObjects
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Person> people = new List<Person>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] info = input.Split();
+                people.Add(new Person(info[0], int.Parse(info[1]), info[2]));
+            }
+            int targetPersonIndex = int.Parse(Console.ReadLine()) - 1;
+            Person targetPerson = people[targetPersonIndex];
+
+            if (people.Where(p => p.CompareTo(targetPerson) == 0).Count() > 1)
+            {
+                Console.Write(people.Where(p => p.CompareTo(targetPerson) == 0).Count() + " ");
+                Console.Write(people.Where(p => p.CompareTo(targetPerson) != 0).Count() + " ");
+                Console.WriteLine(people.Count());
+            }
+            else
+                Console.WriteLine("No matches");
+        }
+    }
+}
