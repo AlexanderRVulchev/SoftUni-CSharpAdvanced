209635ce92ddd527c1519b1d581776acdba1266b
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-05 19:58:02 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/04. Froggy/Lake.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace _04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int position;
+
+        public List<int> Stones;
+
+        public Lake(int[] stones)
+        {
+            this.Stones = new List<int>(stones);
+            this.position = 0;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.Stones.Count; i++)
+            {
+                int current = this.Stones[position];
+                if (this.position % 2 == 0) this.position += 2;
+                else if (this.position % 2 == 1) this.position -= 2;
+
+                if (this.position == this.Stones.Count)
+                    this.position--;
+                else if (this.position > this.Stones.Count)
+                    this.position -= 3;
+
+                yield return current;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+    }
+}
---
+++ Iterators and Comparators - Exercise/04. Froggy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] stones = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Lake lake = new Lake(stones);
+            Console.WriteLine(String.Join(", ", lake));
+        }
+    }
+}
