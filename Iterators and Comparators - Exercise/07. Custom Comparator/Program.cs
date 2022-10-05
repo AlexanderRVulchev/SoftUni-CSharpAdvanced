8b303b2eaf7cb1a33dd71877feef342dc177788e
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-05 19:59:18 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/07. Custom Comparator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CustomComparator
+{
+    public class NumComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x % 2 == 0 && Math.Abs(y) % 2 == 1)
+                return -1;
+            if (Math.Abs(x) % 2 == 1 && y % 2 == 0)
+                return 1;
+            return x.CompareTo(y);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] inputArray = Console.ReadLine()
+                              .Split()
+                              .Select(x => int.Parse(x))
+                              .ToArray();
+            NumComparer comparer = new NumComparer();
+            Array.Sort(inputArray, comparer);
+            Console.WriteLine(string.Join(" ", inputArray));
+        }
+    }
+}
