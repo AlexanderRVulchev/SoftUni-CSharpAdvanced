3baf654b8686e8514c8cc842d1fea8077a7a3a75
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-10 17:52:58 +0300

    Add files via upload

---
+++ Basic Algorithms - Exercise/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿//Create a program that sums all elements in an array. Use recursion.
+
+using System;
+using System.Linq;
+
+namespace _01._Recursive_Array_Sum
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] nums = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            Console.WriteLine(Sum(nums, 0));
+        }
+
+        static int Sum(int[] nums, int index)
+        {
+            if (index == nums.Length - 1)
+                return nums[nums.Length - 1];
+            else
+                return nums[index] + Sum(nums, ++index);
+        }
+    }
+}
