869dca82a681533528f7402cdedb96292bf5e6e6
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-10 17:53:21 +0300

    Add files via upload

---
+++ Basic Algorithms - Exercise/02. Recursive Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿//Create a program that finds the factorial of a given number. Use recursion.
+
+using System;
+
+namespace _02._Recursive_Factorial
+{
+    public class Program
+    {
+        static void Main()
+        {
+            long num = long.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(num));
+        }
+
+        static long Factorial(long n)
+        {
+            if (n == 1) return 1;
+            else return n * Factorial(n - 1);
+        }
+    }
+}
