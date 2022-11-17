73f7ab0e91383de7f85304d1bb22a83b8b840549
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-17 18:11:00 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/03. Retake Exam - 13 April 2022/03. Zoo/Animal.cs
@@ -0,0 +1,21 @@
+﻿namespace Zoo
+{
+    public class Animal
+    {
+        public string Species { get; set; }
+        public string Diet { get; set; }
+        public double Weight { get; set; }
+        public double Length { get; set; }
+
+        public Animal(string species, string diet, double weight, double length)
+        {
+            this.Species = species;
+            this.Diet = diet;
+            this.Weight = weight;
+            this.Length = length;
+        }
+
+        public override string ToString()
+            => $"The {this.Species} is a {this.Diet} and weighs {this.Weight} kg.";
+    }
+}
