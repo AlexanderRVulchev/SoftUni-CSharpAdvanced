04bd63da623d5049e7d5244851373b401e0dd49e
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-18 18:26:22 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/04. Exam - 20 February 2022/03. Fishing Net/Fish.cs
@@ -0,0 +1,19 @@
+﻿namespace FishingNet
+{
+    public class Fish
+    {        
+        public string FishType { get; set; }
+        public double Length { get; set; }
+        public double Weight { get; set; }
+
+        public Fish(string fishType, double length, double weight)
+        {
+            this.FishType = fishType;
+            this.Length = length;
+            this.Weight = weight;
+        }
+
+        public override string ToString()
+            => $"There is a {this.FishType}, {this.Length} cm. long, and {this.Weight} gr. in weight.";
+    }
+}
