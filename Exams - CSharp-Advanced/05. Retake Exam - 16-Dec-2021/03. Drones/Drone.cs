9965b80625f42f9bc071f76dcba8029687355206
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-19 17:30:40 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/05. Retake Exam - 16-Dec-2021/03. Drones/Drone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Drones
+{
+    public class Drone
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Range { get; set; }
+        public bool Available { get; set; }
+
+        public Drone(string name, string brand, int range)
+        {
+            this.Available = true;
+            this.Name = name;
+            this.Brand = brand;
+            this.Range = range;
+        }
+
+        public override string ToString()
+         => $"Drone: {this.Name}" + Environment.NewLine +
+            $"Manufactured by: {this.Brand}" + Environment.NewLine +
+            $"Range: {this.Range} kilometers";
+    }
+}
