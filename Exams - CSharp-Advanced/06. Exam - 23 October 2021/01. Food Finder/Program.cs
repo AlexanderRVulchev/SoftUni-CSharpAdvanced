50209c40503cc7508d7ae2c3a3f12d4f8038d49e
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-20 08:06:08 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/06. Exam - 23 October 2021/01. Food Finder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Food_Finder
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var vowels = new Queue<char>(string.Join("", Console.ReadLine().Split()).ToCharArray());
+            var consonants = new Stack<char>(string.Join("", Console.ReadLine().Split()).ToCharArray());
+            HashSet<string> wordsSet = new HashSet<string>(new string[] { "pear", "flour", "pork", "olive" });
+            HashSet<char> usedLetters = new HashSet<char>();
+            
+            while (consonants.Any())
+            {
+                usedLetters.Add(consonants.Pop());
+                usedLetters.Add(vowels.Peek());
+                vowels.Enqueue(vowels.Dequeue());
+            }
+
+            List<string> validWords = new List<string>();
+            foreach (string word in wordsSet)
+            {
+                if (string.Join("", word.Intersect(usedLetters)) == word)
+                    validWords.Add(word);
+            }
+
+            Console.WriteLine($"Words found: {validWords.Count} ");
+            Console.WriteLine(string.Join(Environment.NewLine, validWords));
+        }
+    }
+}
