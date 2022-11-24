0fe290181e8fd94ff664fe9aefc96079cc18b5c9
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-24 17:54:04 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/07. Retake Exam - 18 August 2021/03. Street Racing/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace StreetRacing
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string LicensePlate { get; set; }
+        public int HorsePower { get; set; }
+        public double Weight { get; set; }
+
+        public Car(string make, string model, string licensePlate, int horsePower, double weight)
+        {
+            this.Weight = weight;
+            this.Make = make;
+            this.Model = model;
+            this.LicensePlate = licensePlate;
+            this.HorsePower = horsePower;
+        }
+
+        public override string ToString()
+            => $"Make: {this.Make}" + Environment.NewLine +
+               $"Model: {this.Model}" + Environment.NewLine +
+               $"License Plate: {this.LicensePlate}" + Environment.NewLine +
+               $"Horse Power: {this.HorsePower}" + Environment.NewLine +
+               $"Weight: {this.Weight}";
+    }
+}
