3ff68978c5b97e7c6ad93e88516d8301fff829ab
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-24 17:52:44 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/07. Retake Exam - 18 August 2021/01. Birthday Celebration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Birthday_Celebration
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var guests = new Stack<int>(Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray().Reverse());
+            var plates = new Stack<int>(Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray());
+            int wastedFood = 0;
+
+            while (guests.Any() && plates.Any())
+            {
+                int currentGuest = guests.Pop();
+                int currentPlate = plates.Pop();
+                currentGuest -= currentPlate;
+                if (currentGuest <= 0)
+                    wastedFood -= currentGuest;
+                else
+                    guests.Push(currentGuest);
+            }
+
+            if (plates.Any())
+                Console.WriteLine($"Plates: {string.Join(" ", plates)}");
+            else
+                Console.WriteLine($"Guests: {string.Join(" ", guests)}");
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
+        }
+    }
+}
