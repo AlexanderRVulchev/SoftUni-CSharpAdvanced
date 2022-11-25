2c2dce7378c29e227a1b1c2f756d60d2c6ce98d7
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-25 13:03:01 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/08. Exam - 26 June 2021/01.Masterchef/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Masterchef
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Queue<int> ingredients = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Stack<int> freshness = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Dictionary<int, string> listOfMeals = GetListOfMeals();
+            SortedDictionary<string, int> cookedDishes = new SortedDictionary<string, int>();
+
+            while (ingredients.Any() && freshness.Any())
+            {
+                int currentIngredient = ingredients.Dequeue();
+                if (currentIngredient == 0) continue;
+
+                int currentFreshness = freshness.Pop();                
+                int product = currentIngredient * currentFreshness;
+                if (listOfMeals.ContainsKey(product))
+                {
+                    string meal = listOfMeals[product];
+                    if (!cookedDishes.ContainsKey(meal))
+                        cookedDishes.Add(meal, 0);
+                    cookedDishes[meal]++;
+                }
+                else
+                    ingredients.Enqueue(currentIngredient + 5);
+            }
+
+            if (cookedDishes.Count >= 4)
+                Console.WriteLine($"Applause! The judges are fascinated by your dishes!");
+            else
+                Console.WriteLine($"You were voted off. Better luck next year.");
+
+            if (ingredients.Any())
+                Console.WriteLine($"Ingredients left: {ingredients.Sum()}");
+            if (cookedDishes.Count > 0)
+                foreach (var dish in cookedDishes)
+                    Console.WriteLine($" # {dish.Key} --> {dish.Value}");
+        }
+
+        static Dictionary<int, string> GetListOfMeals()
+            => new Dictionary<int, string>()
+            {
+                { 150, "Dipping sauce" },
+                { 250, "Green salad" },
+                { 300, "Chocolate cake" },
+                { 400,  "Lobster" }
+            };
+    }
+}
