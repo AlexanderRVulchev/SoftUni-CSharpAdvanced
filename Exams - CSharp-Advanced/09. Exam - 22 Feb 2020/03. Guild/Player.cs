9a4629b0e1372cf6e7013ac5df3a94cab3538acc
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-26 21:17:27 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/09. Exam - 22 Feb 2020/03. Guild/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Guild
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; }
+        public string Description { get; set; }
+
+        public Player(string name, string @class)
+        {
+            this.Name = name;
+            this.Class = @class;
+            this.Rank = "Trial";
+            this.Description = "n/a";
+        }
+
+        public override string ToString()
+            => $"Player {this.Name}: {this.Class}" + Environment.NewLine +
+               $"Rank: {this.Rank}" + Environment.NewLine +
+               $"Description: {Description}";
+    }
+}
