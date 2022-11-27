d4fef113bcb8c6d2068fc05625c4a9e24c2d0a02
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-27 17:54:16 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/10. Retake Exam - 14 April 2021/03. Cocktail Party/Ingredient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace CocktailParty
+{
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public int Alcohol { get; set; }
+        public int Quantity { get; set; }
+
+        public Ingredient(string name, int alcohol, int quantity)
+        {
+            this.Name = name;
+            this.Alcohol = alcohol;
+            this.Quantity = quantity;
+        }
+
+        public override string ToString()
+            => $"Ingredient: {this.Name}" + Environment.NewLine +
+               $"Quantity: {this.Quantity}" + Environment.NewLine +
+               $"Alcohol: {this.Alcohol}";
+    }
+}
