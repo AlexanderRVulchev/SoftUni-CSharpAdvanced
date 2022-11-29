adb5246f82e73f3e6fde9b09672047ed99a7cf90
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-29 20:00:12 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/12. Retake Exam - 16 December 2020/03. Openning/Bakery.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private readonly List<Employee> data;
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return this.data.Count; } }
+
+        public Bakery(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.data = new List<Employee>();
+        }
+
+        public void Add(Employee employee)
+        {
+            if (this.Count < this.Capacity)
+                this.data.Add(employee);
+        }
+
+        public bool Remove(string name)
+            => this.data.Remove(this.data.Find(e => e.Name == name));
+
+        public Employee GetOldestEmployee()
+            => this.data.OrderByDescending(e => e.Age).First();
+
+        public Employee GetEmployee(string name)
+            => this.data.Find(e => e.Name == name);
+
+        public string Report()
+            => $"Employees working at Bakery {this.Name}:" +
+               Environment.NewLine +
+               string.Join(Environment.NewLine, this.data);
+    }
+}
---
+++ Exams - CSharp-Advanced/12. Retake Exam - 16 December 2020/03. Openning/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+
+        public Employee(string name, int age, string country)
+        {
+            this.Age = age;
+            this.Country = country;
+            this.Name = name;
+        }
+
+        public override string ToString()
+            => $"Employee: {this.Name}, {this.Age} ({this.Country})";
+    }
+}
