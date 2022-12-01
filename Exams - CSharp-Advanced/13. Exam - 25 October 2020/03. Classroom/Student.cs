0a0ab046e8aea6958f633acce73c02c97b00f140
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-01 19:07:25 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/13. Exam - 25 October 2020/03. Classroom/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace ClassroomProject
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+
+        public Student(string firstName, string LastName, string subject)
+        {
+            this.FirstName = firstName;
+            this.LastName = LastName;
+            this.Subject = subject;
+        }
+
+        public override string ToString()
+            => $"Student: First Name = {this.FirstName}, Last Name = {this.LastName}, Subject = {this.Subject}";
+    }
+}
