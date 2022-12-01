294a3c387edd8d249f51003bd1fecd51ee78bf19
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-01 19:06:09 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/13. Exam - 25 October 2020/01. Scheduling/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Scheduling
+{
+    internal class Program
+    {
+        static void Main()
+        {            
+            Stack<int> tasks = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Queue<int> threads = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int taskToKill = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                int task = tasks.Peek();
+                int thread = threads.Peek();
+                if (task == taskToKill) 
+                    break;
+
+                tasks.Pop();
+                threads.Dequeue();
+
+                if (thread < task)
+                    tasks.Push(task);
+            }
+            Console.WriteLine($"Thread with value {threads.Peek()} killed task {taskToKill}");
+            Console.WriteLine(String.Join(" ", threads));
+        }
+    }
+}
