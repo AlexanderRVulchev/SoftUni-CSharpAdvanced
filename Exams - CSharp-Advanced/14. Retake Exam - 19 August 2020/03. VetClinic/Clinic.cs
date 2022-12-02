0385b7db05ee3e6c60fa9eed9818183568e290c9
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-02 18:06:01 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/14. Retake Exam - 19 August 2020/03. VetClinic/Clinic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private readonly List<Pet> data;
+
+        public int Capacity { get; set; }
+        public int Count { get { return this.data.Count; } }
+
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            this.data = new List<Pet>();
+        }
+
+        public void Add(Pet pet)
+        {
+            if (this.Count < this.Capacity) this.data.Add(pet);
+        }
+
+        public bool Remove(string name)
+            => this.data.Remove(this.data.Find(p => p.Name == name));
+
+        public Pet GetPet(string name, string owner)
+            => this.data.Find(p => p.Name == name && p.Owner == owner);
+
+        public Pet GetOldestPet()
+            => this.data.OrderByDescending(p => p.Age).FirstOrDefault();
+
+        public string GetStatistics()
+            => $"The clinic has the following patients:" + Environment.NewLine +
+            string.Join(Environment.NewLine, this.data.Select(p => $"Pet {p.Name} with owner: {p.Owner}"));
+    }
+}
---
+++ Exams - CSharp-Advanced/14. Retake Exam - 19 August 2020/03. VetClinic/Pet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace VetClinic
+{
+    public class Pet
+    {        
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Owner { get; set; }
+
+        public Pet(string name, int age, string owner)
+        {
+            this.Age = age;
+            this.Owner = owner;
+            this.Name = name;
+        }
+
+        public override string ToString()
+            => $"Name: {this.Name} Age: {this.Age} Owner: {this.Owner}";
+    }
+}
