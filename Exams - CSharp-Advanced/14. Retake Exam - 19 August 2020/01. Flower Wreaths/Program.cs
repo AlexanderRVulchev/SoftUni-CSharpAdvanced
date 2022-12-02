1ea31249193eef825f16bbaba39a3794575147f8
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-02 18:05:25 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/14. Retake Exam - 19 August 2020/01. Flower Wreaths/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Flower_Wreaths
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            int storedFlowers = 0;
+            int wreaths = 0;
+
+            while (lilies.Any() && roses.Any())
+            {
+                int currentLilies = lilies.Peek();
+                int currentRoses = roses.Peek();
+                int sum = currentLilies + currentRoses;
+                if (sum > 15)
+                {
+                    lilies.Push(lilies.Pop() - 2);
+                    continue;
+                }
+                else if (sum < 15) storedFlowers += sum;
+                else wreaths++;
+
+                lilies.Pop();
+                roses.Dequeue();
+            }
+            wreaths += storedFlowers / 15;
+
+            if (wreaths >= 5)
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            else
+                Console.WriteLine($"You didn't make it, you need {5 - wreaths} wreaths more!");
+        }
+    }
+}
