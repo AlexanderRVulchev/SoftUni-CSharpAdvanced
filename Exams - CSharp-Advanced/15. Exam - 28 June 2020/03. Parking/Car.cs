e98522b2a176385030e43827a4af63fb885de088
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-03 21:41:03 +0200

    Add files via upload

---
+++ Exams - CSharp-Advanced/15. Exam - 28 June 2020/03. Parking/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Parking
+{
+    public class Car
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public Car(string manufacturer, string model, int year)
+        {
+            this.Model = model;
+            this.Manufacturer = manufacturer;
+            this.Year = year;
+        }
+
+        public override string ToString()
+            => $"{this.Manufacturer} {this.Model} ({this.Year})";
+    }
+}
---
+++ Exams - CSharp-Advanced/15. Exam - 28 June 2020/03. Parking/Parking.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parking
+{
+    public class Parking
+    {
+        private readonly List<Car> data;
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return this.data.Count; } }
+
+        public Parking(string type, int capacity)
+        {
+            this.Capacity = capacity;
+            this.Type = type;
+            this.data = new List<Car>();
+        }
+
+        public void Add(Car car)
+        {
+            if (this.Count < this.Capacity)
+                this.data.Add(car);
+        }
+
+        public bool Remove(string manufacturer, string model)
+            => this.data.Remove(this.data.Find(c => c.Manufacturer == manufacturer && c.Model == model));
+
+        public Car GetLatestCar()
+            => this.data.OrderByDescending(c => c.Year).FirstOrDefault();
+
+        public Car GetCar(string manufacturer, string model)
+            => this.data.Find(c => c.Manufacturer == manufacturer && c.Model == model);
+
+        public string GetStatistics()
+            => $"The cars are parked in {this.Type}:" +
+               Environment.NewLine +
+               string.Join(Environment.NewLine, this.data);
+    }
+}
